aabcb57787071f415454bd1e1c46b77f2583b057
adw-ait <adwaityugu@gmail.com>
2022-01-15 21:04:51 +0530

    sumof3and5

---
+++ multipleof3and5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    int sum = 0;
+
+    for(int i = 0; i < 1000; i++){
+        if(i % 3 == 0 || i % 5 == 0){
+            sum = sum + i;
+        }
+    }
+
+    cout<<"\nSum is : "<<sum;
+    return 0;
+}
